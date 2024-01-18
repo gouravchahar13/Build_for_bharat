827d2458705930707529606e6390ed18878fb08a
gouravchahar13 <1bi22cd014@bit-bangalore.edu.in>
2024-01-18 22:23:43 +0530

    updated

--- home/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-
-def home(request):
-    context={'title':'Ecom'}
-    return render(request,'home.html',context)
--- web_set/wsgi.py
+++ web_set/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for ecom_website project.
+WSGI config for web_set project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ecom_website.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'web_set.settings')
 
 application = get_wsgi_application()
